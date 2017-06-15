ab677de45b7be67d99115b3f8c0bb0ab55f73c33
Kanishka <vkanishk@ford.com>
2017-06-15 13:05:44 +0530

    Documentation change (#30)
    * Documentation and copyright changes
    
    * adding gitignore to ignore the files not needed in every commit
    
    * Changes in changelog
    
    * removing xcuserdata from project file

--- openXCiOSFrameworkTests/openXCiOSFrameworkTests.swift
+++ openXCiOSFrameworkTests/openXCiOSFrameworkTests.swift
@@ -3,7 +3,7 @@
 //  openXCiOSFrameworkTests
 //
 //  Created by Kanishka, Vedi (V.) on 16/11/16.
-//  Copyright © 2016 Bug Labs. All rights reserved.
+//  Copyright (c) 2016 Ford Motor Company Licensed under the BSD license.
 //
 
 import XCTest
